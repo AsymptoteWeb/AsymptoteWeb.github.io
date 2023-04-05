9d40d5f6a7e698ac75eacf9bcfd65b26fb41ca18
kbrac <kktkate28@gmail.com>
2023-04-06 02:37:34 +0800

    add sizer for this

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,7 @@ import 'package:companywebapp/homescreen_v2.dart';
 import 'package:flutter/material.dart';
 import 'Additional/responsive.dart';
 import 'Additional/constants.dart';
+import 'homedscreen_v4.dart';
 import 'homescreen.dart';
 import 'homescreen_v3.dart';
 
@@ -16,7 +17,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: HomeScreen_v3(),
+        body: HomeScreen_v4(),
         // ResponsiveWidget(
         //   largeScreen: HomeScreen(),
         //   smallScreen: HomeScreen(),
